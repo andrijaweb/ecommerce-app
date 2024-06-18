49b8a892c6491fee3ac8acf59cae1abd54674780
andrijaweb <andrijawebd@gmail.com>
2024-06-18 12:17:40 +0200

    Clean code, added global type for Option, started sorting component

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,4 +2,9 @@ import type { Database } from "../types/database.types";
 
 declare global {
   type product = Database["public"]["Tables"]["products"]["Row"];
+
+  type Option = {
+    value: string;
+    label: string;
+  };
 }
