f46509d1800c0d7d21a1e8d2d3fb4f0e59dd8f2d
andrijaweb <andrijawebd@gmail.com>
2024-06-22 14:05:58 +0200

    Added Logo prop for choosing either dark or light logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 
-const Logo = () => {
-  const isDarkMode = false;
+type LogoProps = {
+  isDark: boolean;
+};
 
-  const src = isDarkMode ? "./logo-dark.svg" : "/logo-light.svg";
+const Logo = ({ isDark = false }: LogoProps) => {
+  const src = isDark ? "./logo-dark.svg" : "/logo-light.svg";
 
   return (
-    <Link className="my-auto h-9" to="/">
+    <Link className="inline-block my-auto h-9" to="/">
       <img src={src} alt="logo" className="w-full h-full" />
     </Link>
   );
