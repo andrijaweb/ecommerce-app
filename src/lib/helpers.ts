ab6eedd1ae2cbefa358bd5c0d539890ae0a67d62
andrijaweb <andrijawebd@gmail.com>
2024-06-26 11:30:55 +0200

    Added redux selectors to cartoverview, all calculations are shown

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,3 +1,5 @@
+import { FLAT_SHIPPING_RATE, TAX_RATE } from "./constants";
+
 export function capitalizeFirstLetter(
   category: string | null
 ): string | undefined {
@@ -6,3 +8,19 @@ export function capitalizeFirstLetter(
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join(" ");
 }
+
+export const calculateShipping = (): number => {
+  return FLAT_SHIPPING_RATE;
+};
+
+export const calculateTax = (subtotal: number): number => {
+  return +(subtotal * TAX_RATE).toFixed(2);
+};
+
+export const calculateGrandTotal = (
+  subtotal: number,
+  shipping: number,
+  tax: number
+): number => {
+  return subtotal + shipping + tax;
+};
