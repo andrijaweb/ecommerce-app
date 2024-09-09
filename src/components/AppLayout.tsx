aba3023914acce5f34179a184d47a570e5d6b2c2
andrijaweb <andrijawebd@gmail.com>
2024-09-09 11:54:33 +0200

    Removed uneccessary code

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,14 +1,8 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
-import { useUser } from "@/features/authentication/useUser";
-import FullPageSpinner from "./FullPageSpinner";
 
 const AppLayout = () => {
-  const { isLoading } = useUser();
-
-  if (isLoading) return <FullPageSpinner />;
-
   return (
     <div>
       <Header />
